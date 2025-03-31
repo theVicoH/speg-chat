99c53d41898396a9e7ab5092fe7b14242446d31c
Vitomir Laces <vitomirlaces@gmail.com>
2025-03-31 16:44:33 +0200

    refactor : refactor dump.sql

--- backend/dump/init.sql
+++ backend/dump/init.sql
@@ -1,49 +1,68 @@
 CREATE DATABASE IF NOT EXISTS `speg-db`;
+USE `speg-db`;
 
-use `speg-db`;
+CREATE TABLE IF NOT EXISTS `roles` (
+    `id` BIGINT AUTO_INCREMENT NOT NULL,
+    `role` CHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
-CREATE TABLE IF NOT EXISTS `user`(
+CREATE TABLE IF NOT EXISTS `rooms_types` (
+    `id` BIGINT AUTO_INCREMENT NOT NULL,
+    `type` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `bans_types` (
+    `id` BIGINT AUTO_INCREMENT NOT NULL,
+    `type` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
     `id` BIGINT AUTO_INCREMENT NOT NULL,
     `role_id` BIGINT NOT NULL,
     `username` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    PRIMARY KEY(`id`)
+    PRIMARY KEY (`id`),
+    CONSTRAINT `user_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`)
 );
-CREATE TABLE IF NOT EXISTS `role`(
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `role` CHAR(255) NOT NULL,
-    PRIMARY KEY(`id`)
-);
-CREATE TABLE IF NOT EXISTS `room`(
+
+CREATE TABLE IF NOT EXISTS `rooms` (
     `id` BIGINT AUTO_INCREMENT NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `created_at` DATETIME NOT NULL,
     `updated_at` DATETIME NOT NULL,
     `creator_id` BIGINT NOT NULL,
     `room_type_id` BIGINT NOT NULL,
-    PRIMARY KEY(`id`)
+    PRIMARY KEY (`id`),
+    CONSTRAINT `room_creator_id_foreign` FOREIGN KEY (`creator_id`) REFERENCES `users`(`id`),
+    CONSTRAINT `room_room_type_id_foreign` FOREIGN KEY (`room_type_id`) REFERENCES `rooms_types`(`id`)
 );
-CREATE TABLE IF NOT EXISTS `message`(
+
+CREATE TABLE IF NOT EXISTS `messages` (
     `id` BIGINT AUTO_INCREMENT NOT NULL,
     `message` TEXT NOT NULL,
     `user_id` BIGINT NOT NULL,
     `created_at` DATETIME NOT NULL,
     `updated_at` DATETIME NOT NULL,
     `room_id` BIGINT NOT NULL,
-    PRIMARY KEY(`id`)
+    PRIMARY KEY (`id`),
+    CONSTRAINT `message_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    CONSTRAINT `message_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`)
 );
-CREATE TABLE IF NOT EXISTS `user_room`(
+
+CREATE TABLE IF NOT EXISTS `users_rooms` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user_id` BIGINT NOT NULL,
     `room_id` BIGINT NOT NULL,
-    `role_id` BIGINT NOT NULL
+    `role_id` BIGINT NOT NULL,
+    CONSTRAINT `user_room_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    CONSTRAINT `user_room_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`),
+    CONSTRAINT `user_room_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`)
 );
-CREATE TABLE IF NOT EXISTS `room_type`(
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
-    PRIMARY KEY(`id`)
-);
-CREATE TABLE IF NOT EXISTS `ban`(
+
+CREATE TABLE IF NOT EXISTS `bans` (
     `id` BIGINT AUTO_INCREMENT NOT NULL,
     `ban_type_id` BIGINT NOT NULL,
     `user_id` BIGINT NOT NULL,
@@ -51,38 +70,14 @@ CREATE TABLE IF NOT EXISTS `ban`(
     `created_at` DATETIME NOT NULL,
     `expires_at` DATETIME NOT NULL,
     `reason` TEXT NOT NULL,
-    PRIMARY KEY(`id`)
+    PRIMARY KEY (`id`),
+    CONSTRAINT `ban_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    CONSTRAINT `ban_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`),
+    CONSTRAINT `ban_ban_type_id_foreign` FOREIGN KEY (`ban_type_id`) REFERENCES `bans_types`(`id`)
 );
-CREATE TABLE IF NOT EXISTS `ban_type`(
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
-    PRIMARY KEY(`id`)
-);
-ALTER TABLE
-    `ban` ADD CONSTRAINT `ban_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `room` ADD CONSTRAINT `room_room_type_id_foreign` FOREIGN KEY(`room_type_id`) REFERENCES `room_type`(`id`);
-ALTER TABLE
-    `user_room` ADD CONSTRAINT `user_room_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `room` ADD CONSTRAINT `room_creator_id_foreign` FOREIGN KEY(`creator_id`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `user_room` ADD CONSTRAINT `user_room_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `role`(`id`);
-ALTER TABLE
-    `user` ADD CONSTRAINT `user_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `role`(`id`);
-ALTER TABLE
-    `user_room` ADD CONSTRAINT `user_room_room_id_foreign` FOREIGN KEY(`room_id`) REFERENCES `room`(`id`);
-ALTER TABLE
-    `message` ADD CONSTRAINT `message_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `ban` ADD CONSTRAINT `ban_room_id_foreign` FOREIGN KEY(`room_id`) REFERENCES `room`(`id`);
-ALTER TABLE
-    `ban` ADD CONSTRAINT `ban_ban_type_id_foreign` FOREIGN KEY(`ban_type_id`) REFERENCES `ban_type`(`id`);
-ALTER TABLE
-    `message` ADD CONSTRAINT `message_room_id_foreign` FOREIGN KEY(`room_id`) REFERENCES `room`(`id`);
 
-INSERT INTO `role` (role) VALUES ('administrator'), ('moderator'), ('basic');
+INSERT INTO `roles` (`role`) VALUES ('administrator'), ('moderator'), ('basic');
 
-INSERT INTO `room_type` (type) VALUES ('public'), ('private');
+INSERT INTO `rooms_types` (`type`) VALUES ('public'), ('private');
 
-INSERT INTO `ban_type` (type) VALUES ('temporary'), ('definitive');
+INSERT INTO `bans_types` (`type`) VALUES ('temporary'), ('definitive');
