57b7b4bb4597660de3b5f9bcdfb6b126d7793121
Hikyy <mabroukr1997@gmail.com>
2025-04-01 00:13:34 +0200

    feat: update database schema to use INT for IDs and add contacts table

--- backend/dump/init.sql
+++ backend/dump/init.sql
@@ -2,78 +2,80 @@ CREATE DATABASE IF NOT EXISTS `speg-db`;
 USE `speg-db`;
 
 CREATE TABLE IF NOT EXISTS `roles` (
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `role` CHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `role` CHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `rooms_types` (
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `type` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `bans_types` (
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `type` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `role_id` BIGINT NOT NULL,
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `role_id` INT NOT NULL,
     `username` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`),
+    `is_online` BOOLEAN NOT NULL DEFAULT FALSE,
     CONSTRAINT `user_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `rooms` (
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
+    `id` BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `created_at` DATETIME NOT NULL,
     `updated_at` DATETIME NOT NULL,
-    `creator_id` BIGINT NOT NULL,
-    `room_type_id` BIGINT NOT NULL,
-    PRIMARY KEY (`id`),
+    `creator_id` INT NOT NULL,
+    `room_type_id` INT NOT NULL,
     CONSTRAINT `room_creator_id_foreign` FOREIGN KEY (`creator_id`) REFERENCES `users`(`id`),
     CONSTRAINT `room_room_type_id_foreign` FOREIGN KEY (`room_type_id`) REFERENCES `rooms_types`(`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `messages` (
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
+    `id` BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `message` TEXT NOT NULL,
-    `user_id` BIGINT NOT NULL,
+    `user_id` INT NOT NULL,
     `created_at` DATETIME NOT NULL,
     `updated_at` DATETIME NOT NULL,
     `room_id` BIGINT NOT NULL,
-    PRIMARY KEY (`id`),
     CONSTRAINT `message_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     CONSTRAINT `message_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `users_rooms` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_id` BIGINT NOT NULL,
+    `id` BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
     `room_id` BIGINT NOT NULL,
-    `role_id` BIGINT NOT NULL,
+    `role_id` INT NOT NULL,
     CONSTRAINT `user_room_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     CONSTRAINT `user_room_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`),
     CONSTRAINT `user_room_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `bans` (
-    `id` BIGINT AUTO_INCREMENT NOT NULL,
-    `ban_type_id` BIGINT NOT NULL,
-    `user_id` BIGINT NOT NULL,
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `ban_type_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
     `room_id` BIGINT NOT NULL,
     `created_at` DATETIME NOT NULL,
     `expires_at` DATETIME NOT NULL,
     `reason` TEXT NOT NULL,
-    PRIMARY KEY (`id`),
     CONSTRAINT `ban_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     CONSTRAINT `ban_room_id_foreign` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`),
     CONSTRAINT `ban_ban_type_id_foreign` FOREIGN KEY (`ban_type_id`) REFERENCES `bans_types`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `contacts` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `user_id` INT NOT NULL,
+    `contact_id` INT NOT NULL,
+    CONSTRAINT `fk_contacts_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    CONSTRAINT `fk_contacts_contact` FOREIGN KEY (`contact_id`) REFERENCES `users`(`id`)
 );
 
 INSERT INTO `roles` (`role`) VALUES ('administrator'), ('moderator'), ('basic');
