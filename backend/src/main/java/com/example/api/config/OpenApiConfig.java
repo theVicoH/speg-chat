c0bf87230c6f22a390b58dc6b9652ee2cb1abca9
Hikyy <mabroukr1997@gmail.com>
2025-04-01 18:09:47 +0200

    feat: add security scheme for JWT authentication in OpenAPI configuration

--- backend/src/main/java/com/example/api/config/OpenApiConfig.java
+++ backend/src/main/java/com/example/api/config/OpenApiConfig.java
@@ -4,18 +4,30 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
-import lombok.extern.slf4j.Slf4j;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        log.info("Initializing OpenAPI configuration");
+        final String securitySchemeName = "bearerAuth";
+        
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Entrez votre JWT token")))
                 .info(new Info()
                         .title("SPEG Chat API")
                         .description("API pour l'application SPEG Chat")
