b6d2511c4dd95cdf9d56257bfab905257ccc0747
Victor <58180744+theVicoH@users.noreply.github.com>
2025-04-02 16:49:26 +0200

    Revert "refacto : user to lombok"
    This reverts commit e54c9cba829406c570fbbc076a7676b6fc13cb68.

--- backend/src/main/java/com/example/api/dtos/RegisterUserDto.java
+++ backend/src/main/java/com/example/api/dtos/RegisterUserDto.java
@@ -1,11 +1,32 @@
 package com.example.api.dtos;
 
-import lombok.Data;
-import lombok.experimental.Accessors;
-
-@Data
-@Accessors(chain = true)
 public class RegisterUserDto {
     private String username;
     private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public RegisterUserDto setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public RegisterUserDto setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterUserDto{" +
+                "password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
