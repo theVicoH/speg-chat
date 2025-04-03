a3c88948c8183a0fe173a7bea9c2782082dd33a6
Victor Huang <huang.victor.mail@gmail.com>
2025-04-03 20:02:16 +0200

    feat : get all public rooms

--- backend/src/main/java/com/example/api/repositories/RoomRepository.java
+++ backend/src/main/java/com/example/api/repositories/RoomRepository.java
@@ -1,6 +1,7 @@
 package com.example.api.repositories;
 
 import com.example.api.entities.Room;
+import com.example.api.entities.RoomType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Integer> {
     List<Room> findAll();
+    List<Room> findByType(RoomType type);
+    List<Room> findByTypeId(Integer typeId);
 }
