56d3a36cc2b3741f73981eef4db512fd621bdb8f
Victor Huang <huang.victor.mail@gmail.com>
2025-04-03 03:36:22 +0200

    feat : join room

--- backend/src/main/java/com/example/api/entities/UserRoom.java
+++ backend/src/main/java/com/example/api/entities/UserRoom.java
@@ -1,58 +1,30 @@
 package com.example.api.entities;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users_rooms")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @ManyToOne
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    @ManyToOne
+    
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "room_id", nullable = false)
     private Room room;
-
-    @ManyToOne
-    @JoinColumn(name = "role_id", nullable = false)
-    private Role role;
-
-    // Getters and Setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Room getRoom() {
-        return room;
-    }
-
-    public void setRoom(Room room) {
-        this.room = room;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
+    
+    @Column(name = "role_id", nullable = false)
+    private Integer roleId;
 }
